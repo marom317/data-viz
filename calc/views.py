d4e1babd677fbf5afd975423a4a48b534ff201d9
marom317 <marom317@gmail.com>
2019-11-06 22:16:22 +0200

    made a name query

--- calc/views.py
+++ calc/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Hey Itzik ! I hope your hand will better soon ! ")
+    query = request.GET.get('name')
+    message = "Hey {} i can change u !".format(query)
+    template = "home.html"
+    context = {
+        'message':message,
+    }
+    return render(request, template, context)
--- marom/settings.py
+++ marom/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'marom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR , 'templats')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
